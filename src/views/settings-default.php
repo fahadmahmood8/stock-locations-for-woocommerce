cf6c6e146fd93a05c836020d44dbc9c119deef5a
WordPress Mechanic <fahadmahmood8@users.noreply.github.com>
2024-05-28 23:57:26 +0500

    2.6.8

--- src/views/settings-default.php
+++
@@ -1,8 +0,0 @@
-<?php if ( ! defined('WPINC') ) die; ?>
-<form method="post" action="options.php">
-<?php
-	settings_fields( 'slw_setting_option_group' );
-	do_settings_sections( 'slw-setting-admin' );
-	submit_button();
-?>
-</form>
