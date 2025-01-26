74de350f439070ac09d0e3cbac4e15e8fa4fa364
WordPress Mechanic <fahad.mahmood.8@gmail.com>
2025-01-26 09:32:30 +0500

    2.7.9

--- src/stock-locations-for-woocommerce.php
+++ src/stock-locations-for-woocommerce.php
@@ -119,7 +119,7 @@ if(!class_exists('SlwMain')) {
 
 	class SlwMain{
 		// versions
-		public           $version  = '2.7.8';
+		public           $version  = '2.7.9';
 		public           $import_export_addon_version = '1.1.1';
 
 		// others
