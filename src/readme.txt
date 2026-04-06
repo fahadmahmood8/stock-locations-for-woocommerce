ccfb85bf7eccf8e9ca1f9739d39ea84345489e25
WordPress Mechanic <fahadmahmood8@users.noreply.github.com>
2026-04-06 13:21:58 +0500

    3.1.2

--- src/readme.txt
+++ src/readme.txt
@@ -4,7 +4,7 @@ Tags: woocommerce, stock, stock locations, simple product, variable products
 Requires at least: 4.9
 Tested up to: 6.9
 Requires PHP: 7.2
-Stable tag: 3.1.0
+Stable tag: __STABLE_TAG__
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- src/stock-locations-for-woocommerce.php
+++ src/stock-locations-for-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       		Stock Locations for WooCommerce
  * Description:       		This plugin will help you to manage WooCommerce Products stocks through locations.
- * Version:					3.1.0
+ * Version:					__STABLE_TAG__
  * Requires at least: 		4.9
  * Requires PHP:      		7.2
  * Author:            		Fahad Mahmood & Alexandre Faustino
@@ -80,7 +80,7 @@ if(!class_exists('SlwMain')) {
 
 	class SlwMain{
 		// versions
-		public           $version  = '3.1.1';
+		public           $version  = '3.1.2';
 		public           $import_export_addon_version = '1.1.1';
 
 		// others
