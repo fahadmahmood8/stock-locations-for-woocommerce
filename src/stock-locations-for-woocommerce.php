16b0dcb9d2af8c14022595f8ffd982a7dce748be
WordPress Mechanic <fahad.mahmood.8@gmail.com>
2026-05-26 17:44:36 +0500

    3.1.6

--- src/stock-locations-for-woocommerce.php
+++ src/stock-locations-for-woocommerce.php
@@ -80,7 +80,7 @@ if(!class_exists('SlwMain')) {
 
 	class SlwMain{
 		// versions
-		public           $version  = '3.1.5';
+		public           $version  = '3.1.6';
 		public           $import_export_addon_version = '1.1.1';
 
 		// others
